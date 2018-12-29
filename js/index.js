e0ccedf73570fa6b758ccb6d1e14877acc00d217
gitvanhov <johntorohov@gmail.com>
2018-12-29 19:29:24 +0700

    Fixed bottom menu and updated menu logic

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
 
-    //menu
-    $('a').click(function(){
+    //top menu
+    $('.topnav a').click(function(){
 
         $('a.active').removeClass('active');
 
         $(this).addClass('active');
 
     });
+
+        //bottom menu
+        $('.bottomnav a').click(function(){
+
+            $('a.active').removeClass('active');
+
+            var topRefId = this.id.replace('bottom', 'top');
+
+            $('#'+topRefId).addClass('active');
+    
+            console.log(this);
+    
+        });
 
 });
